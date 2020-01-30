e13eacb2aafcab7abf7ceec44d3f2ddf32593525
Sebastien Belin <sebastien.belin@adaptris.com>
2020-01-30 03:22:11 +0000

    Upgrade gradle tooling api to 6.1.1
    Fix the resource utils so the last empty line in the gradle template is not lost
    Use junit assertThrows instead of ExpectedException

--- src/test/java/com/adaptris/fxinstaller/utils/ResourceUtilsTest.java
+++ src/test/java/com/adaptris/fxinstaller/utils/ResourceUtilsTest.java
@@ -1,6 +1,7 @@
 package com.adaptris.fxinstaller.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -10,18 +11,23 @@ import java.nio.file.Paths;
 
 import org.junit.Test;
 
-import com.adaptris.fxinstaller.helpers.LicenseLoader;
-
 public class ResourceUtilsTest {
 
   @Test
   public void testToString() throws IOException, URISyntaxException {
-    String licenseText = new LicenseLoader().load();
+    String licenseText = ResourceUtils.toString("/LICENSE.txt");
 
     String expectedText = new String(Files.readAllBytes(Paths.get(getClass().getResource("/LICENSE.txt").toURI())), StandardCharsets.UTF_8);
 
     // Just the first few chars to not deal with the end of line characters
     assertEquals(expectedText.substring(0, 10), licenseText.substring(0, 10));
+  }
+
+  @Test
+  public void testToStringNullName() throws IOException, URISyntaxException {
+    assertThrows(NullPointerException.class, () -> {
+      ResourceUtils.toString(null);
+    });
   }
 
 }
--- src/test/java/com/adaptris/fxinstaller/utils/XmlUtilsTest.java
+++ src/test/java/com/adaptris/fxinstaller/utils/XmlUtilsTest.java
@@ -1,19 +1,15 @@
 package com.adaptris.fxinstaller.utils;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThrows;
 
 import java.net.MalformedURLException;
 
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.w3c.dom.Document;
 import org.xml.sax.SAXParseException;
 
 public class XmlUtilsTest {
-
-  @Rule
-  public ExpectedException expectedEx = ExpectedException.none();
 
   @Test
   public void testGetDocument() throws Exception {
@@ -25,16 +21,16 @@ public class XmlUtilsTest {
 
   @Test
   public void testGetDocumentInvalidXml() throws Exception {
-    expectedEx.expect(SAXParseException.class);
-
-    XmlUtils.getDocument(getClass().getResource("/invalid-xml.xml").toString());
+    assertThrows(SAXParseException.class, () -> {
+      XmlUtils.getDocument(getClass().getResource("/invalid-xml.xml").toString());
+    });
   }
 
   @Test
   public void testGetDocumentInvalidLocation() throws Exception {
-    expectedEx.expect(MalformedURLException.class);
-
-    XmlUtils.getDocument("/invalid-location.xml");
+    assertThrows(MalformedURLException.class, () -> {
+      XmlUtils.getDocument("/invalid-location.xml");
+    });
   }
 
 }
